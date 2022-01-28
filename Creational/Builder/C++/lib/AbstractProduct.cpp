48ea28a7f16cfa5a967b8d093a5da320782d68de
Gustavo <gustavo.nobrega@serttel.com.br>
2022-01-28 08:40:09 +0100

    Updated print format

--- Creational/Builder/C++/lib/AbstractProduct.cpp
+++ Creational/Builder/C++/lib/AbstractProduct.cpp
@@ -11,6 +11,8 @@ void AbstractProduct::operation() const
 {
 	for( std::set<std::string>::iterator it = m_string_set.begin() ; it != m_string_set.end() ; it++)
 	{
-		std::cout << *it << std::endl;
+		std::cout << *it << " ";
 	}
+
+	std::cout << std::endl;
 }
